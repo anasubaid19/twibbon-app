8e3f04ec31f9066ebc08785ea8a7f7c325a9acbe
Anas Ubaid <anasu8480@gmail.com>
2026-08-02 21:59:36 +0700

    chore: hapus scaffold uikit dari repo, jangan ikut deploy

--- uikit/src/routes/ui-kit.tsx
+++
@@ -1,4 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router"
-import { UiKitPage } from "@/components/ui-kit/ui-kit-page"
-
-export const Route = createFileRoute("/ui-kit")({ component: UiKitPage })
